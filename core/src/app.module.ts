7f1db75be8fcb748b5f972b2a64765262b7728d3
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2025-11-08 10:21:52 -0300

    setup config service with validation and db with async initialization

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.getDatabaseConfig(),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [BooksController],
   providers: [BooksService],
 })
---
+++ core/src/config/config.service.ts
@@ -0,0 +1,66 @@
+import * as Joi from 'joi';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+@Injectable()
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid('development', 'production', 'test', 'provision')
+        .default('development'),
+      PORT: Joi.number().default(3000),
+      DB_HOST: Joi.string().default('localhost'),
+      DB_PORT: Joi.number().default(5432),
+      DB_USER: Joi.string().default('user'),
+      DB_PASSWORD: Joi.string().default('password'),
+      DB_NAME: Joi.string().default('db'),
+    });
+
+    const validatedEnvConfig = envVarsSchema.validate(envConfig, {
+      allowUnknown: true,
+      stripUnknown: true,
+    });
+
+    if (validatedEnvConfig.error) {
+      throw new Error(
+        `Config validation error: ${validatedEnvConfig.error.message}`,
+      );
+    }
+
+    return validatedEnvConfig.value as EnvConfig;
+  }
+
+  isProduction(): boolean {
+    return this.envConfig.NODE_ENV === 'production';
+  }
+
+  getDatabaseConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.envConfig.DB_HOST,
+      port: Number(this.envConfig.DB_PORT),
+      username: this.envConfig.DB_USER,
+      password: this.envConfig.DB_PASSWORD,
+      database: this.envConfig.DB_NAME,
+      synchronize: true,
+    };
+  }
+}
