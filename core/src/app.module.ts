a1e617a0526b4be55220d6f4ea12a669f4ee303b
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2025-11-08 13:44:58 -0300

    created separate modules per feature/use-case

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -4,6 +4,10 @@ import { BooksService } from './books/books.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +18,10 @@ import { ConfigService } from './config/config.service';
         configService.getDatabaseConfig(),
       inject: [ConfigService],
     }),
+    AuthModule,
+    AuthorsModule,
+    BooksModule,
+    UsersModule,
   ],
   controllers: [BooksController],
   providers: [BooksService],
